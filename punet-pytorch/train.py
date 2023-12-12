2a013ff4b76623446a60e9c8cbc6103b7e0af11b
Nicolas F. Chaves-de-Plaza <chadepl@gmail.com>
2023-12-12 15:33:01 +0100

    Latest model version

--- punet-pytorch/train.py
+++ punet-pytorch/train.py
@@ -7,6 +7,8 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from punet import ProbabilisticUnet
 from utils import l2_regularisation
 
+import wandb
+
 # TODO:
 # - we want to train for 240k iterations, in the paper they used batch size 32 so 240k its x 32 BS = 7,680,000 images shown to the network in training
 # - 7,680,000 x (1/8882 imgs) = 864.67 is the number of times the whole training set was seen -> this is our number of epochs
@@ -15,73 +17,96 @@ from utils import l2_regularisation
 # -- Confusion matrix 
 
 VAL_FREQ = 10  # run validation every number of epochs
+LEARNING_RATE = 1e-4
+REG_WEIGHT = 1e-5
 
 def train_punet(train_dataset, batch_size_train=1,
                 val_dataset=None, batch_size_val=1,
                 num_classes=1, num_channels_unet=[32, 64, 128, 256],
                 latent_dim=2, no_convs_fcomb=4, beta=10.0,
                 epochs=5, train_id=None, device="cpu"):
+    
+    saving_path = Path(f"training_logs/{train_id}")
+    if saving_path.exists():
+        raise Exception("Path already exists, change train_id")
+    else:
+        saving_path.mkdir(parents=True)
+        saving_path.joinpath("model_checkpoints").mkdir()
 
-    device = torch.device(device)
+        wandb.init(
+        # set the wandb project where this run will be logged
+            project="pytorch-replication",
+            
+            # track hyperparameters and run metadata
+            config={
+            "learning_rate": LEARNING_RATE,
+            "regularization-weight": REG_WEIGHT,
+            "architecture": "PUNet",
+            "dataset": "LIDC",
+            "epochs": epochs,
+            "device": device,
+            "num_classes":num_classes,
+            "num_channels_unet":num_channels_unet,
+            "latent_dim":latent_dim,
+            "no_convs_fcomb":no_convs_fcomb,
+            "beta": beta
+            }
+        )
 
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=batch_size_val, shuffle=False)
+        device = torch.device(device)
 
-    net = ProbabilisticUnet(num_input_channels=1,
-                            num_classes=num_classes,
-                            num_filters=num_channels_unet,
-                            latent_dim=latent_dim,
-                            no_convs_fcomb=no_convs_fcomb,
-                            beta=beta,
-                            device=device)
-    net.to(device)
+        train_dataloader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True)
+        val_dataloader = DataLoader(val_dataset, batch_size=batch_size_val, shuffle=False)
 
-    # Los is defined in each iteration
-    optimizer = torch.optim.Adam(net.parameters(), lr=1e-4, weight_decay=0)
+        net = ProbabilisticUnet(num_input_channels=1,
+                                num_classes=num_classes,
+                                num_filters=num_channels_unet,
+                                latent_dim=latent_dim,
+                                no_convs_fcomb=no_convs_fcomb,
+                                beta=beta,
+                                device=device)
+        net.to(device)
 
-    for epoch in range(1, epochs + 1):
+        # Los is defined in each iteration
+        optimizer = torch.optim.Adam(net.parameters(), lr=LEARNING_RATE, weight_decay=0)
 
-        # Train
-        epoch_losses = []    
-        running_loss = 0
-        net.train()
+        for epoch in range(1, epochs + 1):
 
-        with tqdm(total=int(np.ceil(len(train_dataset)/batch_size_train)), desc=f"Epoch {epoch}/{epochs} (loss: {running_loss})", unit="patch") as pbar:
-            for batch_i, (metadata, img, seg) in enumerate(train_dataloader): 
-                img = img.to(device)
-                seg = seg.to(device)
+            # Train
+            epoch_losses = []    
+            running_loss = 0
+            net.train()
 
-                seg = torch.unsqueeze(seg,1)
-                seg = seg.float()
+            with tqdm(total=int(np.ceil(len(train_dataset)/batch_size_train)), desc=f"Epoch {epoch}/{epochs} (loss: {running_loss})", unit="patch") as pbar:
+                for batch_i, (metadata, img, seg) in enumerate(train_dataloader): 
+                    img = img.to(device)
+                    seg = seg.to(device)
 
-                net.forward(img, seg, training=True)
+                    seg = torch.unsqueeze(seg,1)
+                    seg = seg.float()
 
-                elbo = net.elbo(seg)
-                reg_loss = l2_regularisation(net.posterior) + l2_regularisation(net.prior) + l2_regularisation(net.fcomb.layers)
-                loss_val = -elbo + 1e-5 * reg_loss
+                    net.forward(img, seg, training=True)
 
-                optimizer.zero_grad()
-                loss_val.backward()
-                optimizer.step()
+                    elbo = net.elbo(seg)
+                    reg_loss = l2_regularisation(net.posterior) + l2_regularisation(net.prior) + l2_regularisation(net.fcomb.layers)
+                    loss_val = -elbo + REG_WEIGHT * reg_loss
 
-                epoch_losses.append(loss_val.item())
-                running_loss = np.array(epoch_losses).mean()
+                    optimizer.zero_grad()
+                    loss_val.backward()
+                    optimizer.step()
 
-                pbar.update(1)
-                pbar.set_description(f"Epoch {epoch}/{epochs} (loss: {running_loss:.2f})")
+                    epoch_losses.append(loss_val.item())
+                    running_loss = np.array(epoch_losses).mean()
 
-        # Validate
+                    wandb.log({"epoch": epoch, "acc":net.elbo, "loss": net.reconstruction_loss})
+
+                    pbar.update(1)
+                    pbar.set_description(f"Epoch {epoch}/{epochs} (loss: {running_loss:.2f})")
 
 
-        # Save check point
-        if epoch % VAL_FREQ == 0:
-            print(__file__)
-            test_path = Path(__file__)
-            print(test_path.resolve())
-
-            checkpoint_path = Path(__file__).resolve()
-            checkpoint_path = checkpoint_path.parent
-            checkpoint_path = checkpoint_path.joinpath(f"model_checkpoints/punet_{train_id}")
-            checkpoint_path.mkdir(parents=True, exist_ok=True)
-            state_dict = net.state_dict()
-            torch.save(state_dict, str(checkpoint_path.joinpath(f"checkpoint_epoch-{epoch:02d}.pth")))
+            # Validate
+            if epoch % VAL_FREQ == 0:   
+                print(f"Saving model checkpoint of epoch {epoch:02d}")       
+                checkpoint_path = saving_path.joinpath("model_checkpoints")
+                state_dict = net.state_dict()
+                torch.save(state_dict, str(checkpoint_path.joinpath(f"checkpoint_epoch-{epoch:02d}.pth")))
